08e1e538929d8ba26c66d38fd3b28b002b1d2e65
barkhatov-den <barkhatov.den@gmail.com>
2024-02-05 20:45:22 +0100

    feature: SAPUI5/Fiori Added App controller + Message Toast

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+  'sap/ui/core/mvc/Controller',
+  'sap/m/MessageToast'
+], function(Controller, MessageToast) {
+  'use strict';
+  return Controller.extend('sap.ui.demo.walkthrough.App', {
+    onShowHello: function() {
+      MessageToast.show('Hello there!');
+    }
+  });
+});
