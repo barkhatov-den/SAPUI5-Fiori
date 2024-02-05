3612cbebc6257d5def55666ae03cbf10c922697f
barkhatov-den <barkhatov.den@gmail.com>
2024-02-05 21:25:19 +0100

    feature: SAPUI5/Fiori Added Model + Input (live update + description)

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,19 @@
 sap.ui.define([
   'sap/ui/core/mvc/Controller',
-  'sap/m/MessageToast'
-], function(Controller, MessageToast) {
+  'sap/m/MessageToast',
+  'sap/ui/model/json/JSONModel'
+], function(Controller, MessageToast, JSONModel) {
   'use strict';
   return Controller.extend('sap.ui.demo.walkthrough.App', {
+    onInit: function() {
+      let oData = {
+        recipient: {
+          name: 'Thomas'
+        }
+      };
+      let oModel = new JSONModel(oData);
+      this.getView().setModel(oModel);
+    },
     onShowHello: function() {
       MessageToast.show('Hello there!');
     }
