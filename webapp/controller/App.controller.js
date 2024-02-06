ad45d61958f59b6c79c8f852a5017c314197aa70
barkhatov-den <barkhatov.den@gmail.com>
2024-02-06 18:06:58 +0100

    feature: SAPUI5/Fiori Added i18n(Internationalization)

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,21 +1,36 @@
 sap.ui.define([
   'sap/ui/core/mvc/Controller',
   'sap/m/MessageToast',
-  'sap/ui/model/json/JSONModel'
-], function(Controller, MessageToast, JSONModel) {
+  'sap/ui/model/json/JSONModel',
+  'sap/ui/model/resource/ResourceModel'
+], function(Controller, MessageToast, JSONModel, ResourceModel) {
   'use strict';
   return Controller.extend('sap.ui.demo.walkthrough.App', {
     onInit: function() {
       let oData = {
         recipient: {
-          name: 'Thomas'
+          name: 'Christoph'
         }
       };
       let oModel = new JSONModel(oData);
       this.getView().setModel(oModel);
+      
+      // set i18n model on view
+      let i18nModel = new ResourceModel ({
+        bundleName: 'sap.ui.demo.walkthrough.i18n.i18n',
+        supportedLocales: [''],
+        fallbackLocale: ''
+      });
+      this.getView().setModel(i18nModel, 'i18n');
+      
     },
     onShowHello: function() {
-      MessageToast.show('Hello there!');
+      // read msg from i18n model
+      let oBundle = this.getView().getModel('i18n').getResourceBundle();
+      let sRecipient = this.getView().getModel().getProperty('/recipient/name');
+      let sMsg = oBundle.getText('helloMsg', [sRecipient]);
+      
+      MessageToast.show(sMsg);
     }
   });
 });
