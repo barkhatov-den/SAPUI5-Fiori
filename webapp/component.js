8bd5ca747cd887a3c00bf782a60f1de75bdbf040
barkhatov-den <barkhatov.den@gmail.com>
2024-02-07 15:54:18 +0100

    feature: SAPUI5/Fiori Added dependencies to the manifest.json

--- webapp/component.js
+++ webapp/component.js
@@ -2,16 +2,11 @@ sap.ui.define([
   'sap/ui/core/UIComponent',
   'sap/ui/model/json/JSONModel',
   'sap/ui/model/resource/ResourceModel'
-], function (UIComponent, JSONModel, ResourceModel) {
+], function (UIComponent, JSONModel) {
   'use strict';
   return UIComponent.extend('sap.ui.demo.walkthrough.Component', {
     metadata: {
-      rootView: {
-        'viewName': 'sap.ui.demo.walkthrough.view.App',
-        'type': 'XML',
-        'async': true,
-        'id': 'app'
-      }
+      manifest: "json"
     },
     init: function() {
       // cll the init function of the parent
@@ -24,14 +19,6 @@ sap.ui.define([
       };
       let oModel = new JSONModel(oData);
       this.setModel(oModel);
-      
-      //set i18n model
-      let i18nModel = new ResourceModel ({
-        bundleName: 'sap.ui.demo.walkthrough.i18n.i18n',
-        supportedLocales: [''],
-        fallbackLocale: ''
-      });
-      this.setModel(i18nModel, 'i18n');
     }
   })
 });
