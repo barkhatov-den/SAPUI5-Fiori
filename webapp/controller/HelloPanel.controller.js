3fee48ae994f72afdf009c4f73d8bcd23653e4e5
barkhatov-den <barkhatov.den@gmail.com>
2024-02-08 17:05:37 +0100

    feature: SAPUI5/Fiori Added Dialog and Fragment

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
   'sap/ui/core/mvc/Controller',
   'sap/m/MessageToast',
-], function(Controller, MessageToast) {
+  'sap/ui/core/Fragment'
+], function(Controller, MessageToast, Fragment) {
   'use strict';
   return Controller.extend('sap.ui.demo.walkthrough.controller.HelloPanel', {
     onShowHello: function() {
@@ -10,6 +11,24 @@ sap.ui.define([
       let sMsg = oBundle.getText('helloMsg', [sRecipient]);
       
       MessageToast.show(sMsg);
+    },
+    onOpenDialog: function() {
+      let oView = this.getView();
+      
+      //create the dialog lazily
+      if (!this.byId("helloDialog")) {
+        // load async XML fragment
+        Fragment.load({
+          id: oView.getId(),
+          name: 'sap.ui.demo.walkthrough.view.HelloDialog'
+        }).then(function(oDialog) {
+          // connect dialog to the root view of this component (models, lifecycle)
+          oView.addDependent(oDialog);
+          oDialog.open();
+        });
+      } else {
+        this.byId("helloDialog").open();
+      }
     }
   });
 });
